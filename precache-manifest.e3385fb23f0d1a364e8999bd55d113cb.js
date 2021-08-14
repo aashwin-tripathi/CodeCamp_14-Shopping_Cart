c60bc6f6230381abed252479280dede55721ea0e
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-08-14 12:21:15 +0530

    Updates

---
+++ precache-manifest.e3385fb23f0d1a364e8999bd55d113cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51657cee1567d68007adcfcc096aeae",
+    "url": "/CodeCamp_14-Shopping_Cart/index.html"
+  },
+  {
+    "revision": "37ab055e743255025e8b",
+    "url": "/CodeCamp_14-Shopping_Cart/static/css/main.8db4b5e8.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/CodeCamp_14-Shopping_Cart/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeCamp_14-Shopping_Cart/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ab055e743255025e8b",
+    "url": "/CodeCamp_14-Shopping_Cart/static/js/main.670af2e4.chunk.js"
+  },
+  {
+    "revision": "f0305dd0a765331063cb",
+    "url": "/CodeCamp_14-Shopping_Cart/static/js/runtime-main.15a3cb6a.js"
+  }
+]);
